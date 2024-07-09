4bcee039821551ff37c32f1f9e80a17f6c2058bd
Peurost <dukuliukas@gmail.com>
2024-07-10 01:54:36 +0300

    init: fx

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'PEUREN DEVELOPMENT - peuren.tebex.io'
+version '0.0.0'
+lua54 'yes'
+
+client_script "client/main.lua"
+
+files { "web/build/**/*" }
+
+ui_page "http://localhost:5173/"
+--ui_page "web/build/index.html"
